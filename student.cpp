3514322fe6bd6f0d620f44c3ff7cbb67abef45a6
Nikhil-0725 <nikhilgaikwad0725@gmail.com>
2023-10-21 14:58:11 +0530

    Uploaded

---
+++ student.cpp
@@ -0,0 +1,53 @@
+#include<iostream>
+using namespace std;
+
+
+class student
+{
+	 int studentid;
+	 string studentname;
+	 int marks1,marks2,marks3;	
+};
+
+student()
+{
+	studentid=0;
+	studentname="";
+	marks1=0;
+	marks2=0;
+	marks3=0;
+	
+	
+}
+
+student(int studentid;string studentname;int marks1,int marks2,int marks3)
+{
+	studentid=0;
+	studentname="";
+	marks1=0;
+	marks2=0;
+	marks3=0;
+}
+
+void calculatemarks()
+{
+	int sum;
+	sum=marks1+marks2+marks3;
+	cout<<"Total Marks:"<<sum;
+}
+
+void calculatepercentage()
+{
+	int percentage=(sum/300)*100;
+	cout<<"Percentage: "<<percentage;
+}
+
+
+int main
+{
+	 student.s1;
+	 s1.(100,"James",70,70,70)
+	 
+	 s1.calculatemarks;
+	 s1.calculatepercentage;
+}
